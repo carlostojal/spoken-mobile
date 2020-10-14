127e4e2819c789c35aca93e77241b1578a91a6ad
Carlos Tojal <carlos.tojal@hotmail.com>
2020-10-14 20:21:19 +0100

    remove unused code

--- src/Components/Screens/Login/styles.js
+++ src/Components/Screens/Login/styles.js
@@ -1,21 +1,8 @@
 import { StyleSheet, Dimensions } from "react-native";
 
-import colors from "../../../colors";
-
 export default StyleSheet.create({
 
   area: {
     marginBottom: 20
-  },
-
-  input: {
-    borderWidth: 0.5,
-    borderColor: colors.detail,
-    borderRadius: 30,
-    color: colors.detail,
-    padding: 10,
-    fontSize: 15,
-    fontFamily: "Raleway_400Regular",
-    marginBottom: 10
   }
 });
