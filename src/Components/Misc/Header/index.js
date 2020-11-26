dc8f275c7dd9d17cad0706c56249752402e6023b
Carlos Tojal <carlos.tojal@hotmail.com>
2020-11-26 11:40:40 +0000

    mobile icons

--- src/Components/Misc/Header/index.js
+++ src/Components/Misc/Header/index.js
@@ -3,34 +3,20 @@ import { View, Image } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import colors from "../../../colors";
 import CustomText from "../CustomText";
+import SpokenLogo from "../../../../assets/icons/spoken-logo-cropped.svg";
 
 import styles from "./styles";
 
 export default function Header(props) {
 
-  const [iconsDimensions, setIconsDimensions] = useState({
-    width: 30,
-    height: 30
-  });
-
-  const getHeaderDimensions = (e) => {
-    if(!iconsDimensions.width) {
-      const { nativeEvent } = e;
-      const icon = require("../../../../assets/icons/icons8-menu-30.png");
-      const source = Image.resolveAssetSource(icon);
-      const scale = nativeEvent.layout.height / source.height;
-      const dimensions = {
-        width: (source.width * scale) - 8,
-        height: (source.height * scale) - 8
-      }
-      setIconsDimensions(dimensions);
-    }
+  const onDraw = (e) => {
+    console.log(e.nativeEvent.layout);
   }
 
   return  (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={(e) => onDraw(e)}>
       { props.renderLogo && 
-        <Icon name="logo-twitter" size={45} color={colors.primary} style={{marginRight: 5}} />
+        <SpokenLogo width={70} height={40} style={{marginRight: 10}} />
       }
       <CustomText style={{fontFamily: "Raleway_700Bold", fontSize: 40}}>
         {props.children}
