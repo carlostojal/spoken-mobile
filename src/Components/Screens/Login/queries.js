5005aa2c2d49cf72bd52821a651ad14518193215
Carlos Tojal <carlos.tojal@hotmail.com>
2021-06-09 12:19:22 +0100

    changed the way tokens are stored and received

--- src/Components/Screens/Login/queries.js
+++ src/Components/Screens/Login/queries.js
@@ -4,7 +4,10 @@ export default queries = {
 
   GET_TOKEN: gql`
     query getToken($username: String!, $password: String!, $userPlatform: String, $pushToken: String, $user_lat: Float, $user_long: Float) {
-      getToken(username: $username, password: $password, userPlatform: $userPlatform, pushToken: $pushToken, user_lat: $user_lat, user_long: $user_long) 
+      getToken(username: $username, password: $password, userPlatform: $userPlatform, pushToken: $pushToken, user_lat: $user_lat, user_long: $user_long) {
+        access
+        refresh
+      }
     }
   `,
 
--- src/queries.js
+++ src/queries.js
@@ -4,7 +4,10 @@ export default queries = {
 
   REFRESH_TOKEN: gql`
     query refreshToken($user_lat: Float, $user_long: Float) {
-      refreshToken(user_lat: $user_lat, user_long: $user_long)
+      refreshToken(user_lat: $user_lat, user_long: $user_long) {
+        access
+        refresh
+      }
     }
   `
 }
